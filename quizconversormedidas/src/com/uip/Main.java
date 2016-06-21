780a90152ea5f63a8ce066f46c4b6bb3e8612bd6
axel2929 <axel29@gmail.com>
2016-06-21 14:54:19 -0500

    programa que convierte medidas

---
+++ quizconversormedidas/src/com/uip/Main.java
@@ -0,0 +1,40 @@
+package com.uip;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Main {
+
+
+    public class conversion {
+      public static void conversion() {
+
+          double cm; //Medida en centimetros
+
+          BufferedReader k= new BufferedReader(new InputStreamReader(System.in));
+          System.out.print("Medida en centimetros: ");
+
+          cm= Double.parseDouble(k.readLine());
+
+
+
+    }
+
+
+
+
+
+    public static void main(String[] args) throws Exception {
+        // cambio de cm a pulgadas
+
+
+        double inch; //Medida en pulgadas
+
+        double cm; //Medida en centimetros
+
+        inch = cm / 2.54;
+
+        system.out.println()
+
+    }
+}
